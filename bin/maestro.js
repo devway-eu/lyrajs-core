73fc6bd471887fc26c7149026b08cbb14f520307
Spirule-dev <matthieu.fergola@gmail.com>
2025-12-16 19:35:00 +0100

    fix maestro

--- bin/maestro.js
+++ bin/maestro.js
@@ -1,29 +1,5 @@
 #!/usr/bin/env node
-import { spawn } from "child_process";
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { Kernel } from "../dist/cli/Kernel.js";
 
-// Path to the Kernel
-const kernelPath = join(__dirname, "../dist/cli/Kernel.js");
-
-// Try to find tsx in the user's project
-const tsxCommand = process.platform === "win32" ? "tsx.cmd" : "tsx";
-
-const child = spawn(tsxCommand, [kernelPath, ...process.argv.slice(2)], {
-  stdio: "inherit",
-  shell: true,
-  cwd: process.cwd()
-});
-
-child.on("error", (error) => {
-  console.error("Failed to start maestro:", error.message);
-  console.error("Make sure tsx is installed: npm install tsx");
-  process.exit(1);
-});
-
-child.on("exit", (code) => {
-  process.exit(code || 0);
-});
+Kernel.run(process.argv);
