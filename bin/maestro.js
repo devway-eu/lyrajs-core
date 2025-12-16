71eb6f766f20d1aadcef2f1d355a806f8306c409
Spirule-dev <matthieu.fergola@gmail.com>
2025-12-16 16:20:47 +0100

    update cli

--- bin/maestro.js
+++ bin/maestro.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
+import { register } from "node:module";
+import { pathToFileURL } from "node:url";
+
+// Register tsx for TypeScript support
+register("tsx/esm", pathToFileURL("./"));
+
 import { Kernel } from "../dist/cli/Kernel.js";
 
 Kernel.run(process.argv);
